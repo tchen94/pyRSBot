6b3c8b6f0c238d7e4bd77b39091c3c3a0c7d7d62
Tim Lun Chen <32348217+tchen94@users.noreply.github.com>
2021-08-16 12:07:04 -0400

    Updated magic.py
    Update consist of Bryophyta's staff usage capability and logout function after automation completion

--- magic.py
+++ magic.py
@@ -1,4 +1,3 @@
-
 import random
 import time
 import pyautogui
@@ -14,6 +13,7 @@ from functions import find_Object
 from functions import exit_bank
 from functions import Image_Rec_single
 from functions import deposit_secondItem
+
 global hwnd
 global iflag
 global icoord
@@ -25,9 +25,6 @@ global timer_break
 global ibreak
 
 
-
-
-
 def random_break(start, c):
     global newTime_break
     startTime = time.time()
@@ -36,6 +33,7 @@ def random_break(start, c):
     if startTime - start > c:
         options[a]()
         newTime_break = True
+
 
 def randomizer(timer_breaks, ibreaks):
     global newTime_break
@@ -49,15 +47,18 @@ def randomizer(timer_breaks, ibreaks):
 
     # b = random.uniform(4, 5)
 
+
 def timer():
     startTime = time.time()
     return startTime
+
 
 def random_pause():
     b = random.uniform(20, 250)
     print('pausing for ' + str(b) + ' seconds')
     time.sleep(b)
     newTime_break = True
+
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 iflag = False
@@ -72,9 +73,9 @@ options = {0: random_inventory,
 def high_alch_command():
     # 3rd item
     b = random.uniform(0.33, 0.46)
-    x = random.randrange(1250, 1255) + 5
+    x = random.randrange(1083, 1098) + 5
     print('x: ', x)
-    y = random.randrange(831, 835) + 5
+    y = random.randrange(646, 652) + 5
     print('y: ', y)
     d = random.uniform(0.11, 0.18)
     pyautogui.moveTo(x, y, duration=b)
@@ -83,15 +84,13 @@ def high_alch_command():
     print('alch command clicked')
 
 
-
-
 def high_alch():
     # 3rd item
     b = random.uniform(0.33, 0.46)
     c = random.uniform(1, 1.5)
-    x = random.randrange(1250, 1255)
+    x = random.randrange(1080, 1094)
     print('x: ', x)
-    y = random.randrange(831, 835) + 5
+    y = random.randrange(639, 650) + 6
     print('y: ', y)
     d = random.uniform(0.11, 0.18)
     pyautogui.moveTo(x, y, duration=b)
@@ -100,13 +99,14 @@ def high_alch():
 
     print('alching item')
 
-def high_aclh_loop(vol,bool):
+
+def high_aclh_loop(vol, bool):
     t = vol
     exp = bool
     while t > 0:
         c = random.uniform(1.5, 1.8)
         high_alch_command()
-        #time.sleep(c)
+        # time.sleep(c)
         high_alch()
         c = random.uniform(1.4, 1.9)
         if exp:
@@ -118,34 +118,40 @@ def high_aclh_loop(vol,bool):
             time.sleep(x)
             pyautogui.press('1')
             x = random.uniform(0.5, 0.6)
-            x = random.uniform(0.5, 0.6)
             time.sleep(x)
         time.sleep(c)
         t -= 1
 
+
 def pick_iron_items():
     pick_item(1510 - 1280, 123)
     random_breaks(0.5, 1.5)
+
 
 def pick_bronze_items():
     pick_item(1560 - 1280, 123)
     random_breaks(0.5, 1.5)
     pick_item(1607 - 1280, 123)
     random_breaks(0.1, 0.5)
+
+
 def bank_spot_varrock():
-    find_Object(2) #amber
+    find_Object(2)  # amber
+
 
 def cast_superheat():
     pick_item(2029 - 1280, 573)
+
+
 def pick_ore(type):
     Image_Rec_single(type, 'pick ores', 5, 5, 0.8, 'left', 20, 620, 480, False)
 
 
 def superheat_items(num, bar):
     vol = [13, 27]
-    j = round(num/vol[bar])
+    j = round(num / vol[bar])
     pick_options = {0: pick_bronze_items,
-               1: pick_iron_items}
+                    1: pick_iron_items}
     orelist = ['copper.png', 'iron_ore.png']
     while j > 0:
         bank_spot_varrock()
@@ -166,5 +172,43 @@ def superheat_items(num, bar):
         j -= 1
         random_breaks(0.4, 0.8)
 
-high_aclh_loop(3061, False)
-#superheat_items(100, 1) #100 items iron
+
+def logout():
+    pyautogui.moveTo(1064, 316, duration=1)
+    time.sleep(0.15)
+    pyautogui.click()
+    pyautogui.moveTo(954, 905, duration=1)
+    time.sleep(0.15)
+    pyautogui.click()
+
+def refreshing():
+    pyautogui.moveTo(1027, 970, duration=1)
+    time.sleep(0.15)
+    pyautogui.click()
+    pyautogui.moveTo(932, 735, duration=1)
+    time.sleep(0.15)
+    pyautogui.click()
+    pyautogui.moveTo(991, 968, duration=1)
+    time.sleep(0.15)
+    pyautogui.click()
+    pyautogui.moveTo(930, 643, duration=1)
+    time.sleep(0.15)
+    pyautogui.click()
+    pyautogui.moveTo(984, 643, duration=1)
+    time.sleep(0.15)
+    pyautogui.click()
+    pyautogui.moveTo(985, 643, duration=1)
+    time.sleep(0.15)
+    pyautogui.click()
+    pyautogui.moveTo(1110, 975, duration=1)
+    time.sleep(0.15)
+    pyautogui.click()
+
+cycles = 2
+while cycles > 0:
+    high_aclh_loop(1000, False)
+    refreshing()
+    cycles-=1
+
+
+# superheat_items(100, 1) #100 items iron
